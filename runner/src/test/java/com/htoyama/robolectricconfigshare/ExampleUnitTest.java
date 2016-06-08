e2ee097e0789468d32472a3c6c47d724ef936772
htoyama <duane0728@gmail.com>
2016-06-08 22:58:23 +0900

    initial commit

---
+++ runner/src/test/java/com/htoyama/robolectricconfigshare/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.htoyama.robolectricconfigshare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricCustomRunner.class)
+public class ExampleUnitTest {
+
+  @Test public void addition_isCorrect() throws Exception {
+    assertEquals(4, 2 + 2);
+  }
+
+}
